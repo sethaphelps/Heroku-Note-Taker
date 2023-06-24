20f710bca2e313520c111aaa5cf9818ff460ef3f
sethaphelps <sethaphelps@gmail.com>
2023-06-24 08:14:58 -0700

    worked on delete

---
+++ README.md
@@ -0,0 +1,14 @@
+# Heroku Note Taker 
+
+## Description
+
+For this assignment I needed to create 
+
+## Webpage
+
+Attached is a link to the deployed webpage.
+
+[Repository Link]()
+
+## Sample Link
+
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
+const uuidv4 = require("./helpers/uuid");
+// const router = require("express").Router()
+// const store = require("./public/assets/js/index")
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -19,7 +21,17 @@ app.get("/", (req, res) => {
 
 // Makes route for notes page
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const notes = JSON.parse(data);
+    console.log(notes);
+    res.json(notes);
+      err ? console.log("This is the error.", err) : res.json(JSON.parse(data));
+
+  });
 });
 
 app.post("/api/notes", (req, res) => {
@@ -39,27 +51,15 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
-// app.get("/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/notes.html"));
-// });
-
-// app.post("/api/notes", (req, res) => {
-//   let title = req.body.title;
-//   let text = req.body.text;
-//   let newNote = { title, text, id: uuidv4() };
-
-//   fs.readFile("db/db.json", "utf-8", (err, data) => {
-//     let createdNotes = JSON.parse(data);
-//     createdNotes.push(newNote);
-//     fs.writeFile("db/db.json", JSON.stringify(createdNotes), (err) => {
-//       err
-//         ? console.log("this is the error", err)
-//         : console.log(newNote.title + "has been created");
-//     });
-//     res.sendFile(path.join(__dirname, "public/notes.html"));
-//   });
-// });
+// router.delete("api/notes/:id", (req, res) => {
+//   store 
+//   .deleteNote(req.params.id)
+//   .then(() => res.json({ok:true}))
+//   .catch((err) => res.status(500).json(err))
+// })
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
+
+// module.exports = router
